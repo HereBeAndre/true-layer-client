4fd99e19931f8e6f98774ef2c6607787647c916a
Andrea Tota <andrea.tota6@gmail.com>
2022-05-01 22:11:28 +0200

    Add util function. Remove useless icons from manifest.json

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,2 +1,5 @@
 // USAGE ~ responsible for creating an object of { key: value } pairs from the fields in the form that make up the query.
 export const serializeFormQuery = (key: string, fieldValue: string) => ({ [key]: fieldValue });
+
+// Temporary patch to deal with leading '/' in translated Pokémon descriptions
+export const replaceForwardSlash = (str: string = ''): string => str.replace(/\//g, '');
